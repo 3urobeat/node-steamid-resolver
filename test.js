b04194084737075c5aeba801d1e380c838c53656
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-03-05 11:41:02 +0100

    Updated tests and added finished message

--- test.js
+++ test.js
@@ -1,24 +1,47 @@
 //I hope tests are ok this way? No idea, never added tests to my repos but this seems to make somewhat sense.
 
+var checksAmount = 13; //the amount of checks below to be able to check if all tests are done
+
+var okAmount       = 0;
+var errAmount      = 0;
+
 function response(err, callback, funcname, testval) {
-    if (err) return console.log(`${funcname} error: ${err}`)
-    
     if (callback == testval) { //kind of stupid to rely this test on me not changing my my customURL but also why should I ever
         console.log(`${funcname} ok.`)
+        okAmount++;
     } else {
-        console.log(`${funcname} error!`)
+        console.log(`${funcname} error: ${err}`)
+        errAmount++;
+    }
+
+    if (okAmount + errAmount == checksAmount) {
+        console.log(`\n\nAll tests done. OK: ${okAmount} | Errors: ${errAmount}`)
+        process.exit();
     }
 }
 
 function responseFull(err, callback, funcname, type, testval1, testval2) {
-    if (err) return console.log(`${funcname} error: ${err}`)
-    
     if (type == "profile") {
-        if (Object.keys(callback).length > 5 && callback.customURL[0] == testval1 && callback.steamID64[0] == testval2) console.log(`${funcname} ok.`)
-            else console.log(`${funcname} error!`)
+        if (Object.keys(callback).length > 5 && callback.customURL[0] == testval1 && callback.steamID64[0] == testval2) {
+            console.log(`${funcname} ok.`)
+            okAmount++;
+        } else {
+            console.log(`${funcname} error: ${err}`)
+            errAmount++;
+        }
     } else if (type == "group") {
-        if (Object.keys(callback).length > 5 && callback.groupDetails[0].groupURL[0] == testval1 && callback.groupID64[0] == testval2) console.log(`${funcname} ok.`)
-            else console.log(`${funcname} error!`)
+        if (Object.keys(callback).length > 5 && callback.groupDetails[0].groupURL[0] == testval1 && callback.groupID64[0] == testval2) {
+            console.log(`${funcname} ok.`)
+            okAmount++;
+        } else {
+            console.log(`${funcname} error: ${err}`)
+            errAmount++;
+        }
+    }
+
+    if (okAmount + errAmount == checksAmount) {
+        console.log(`\n\nAll tests done. OK: ${okAmount} | Errors: ${errAmount}`)
+        process.exit();
     }
 }
 
@@ -31,8 +54,10 @@ steamidResolver.steamID64ToCustomUrl("https://steamcommunity.com/profiles/765611
 
 steamidResolver.customUrlTosteamID64("3urobeat", (err, steamID64) => response(err, steamID64, "customUrlTosteamID64", "76561198260031749"))
 steamidResolver.customUrlTosteamID64("https://steamcommunity.com/id/3urobeat", (err, steamID64) => response(err, steamID64, "customUrlTosteamID64", "76561198260031749"))
+steamidResolver.customUrlTosteamID64("https://steamcommunity.com/id/3urobeat/", (err, steamID64) => response(err, steamID64, "customUrlTosteamID64", "76561198260031749"))
 
 steamidResolver.steamID64ToFullInfo("76561198260031749", (err, info) => responseFull(err, info, "steamID64ToFullInfo", "profile", "3urobeat", "76561198260031749"))
+steamidResolver.steamID64ToFullInfo("https://steamcommunity.com/profiles/76561198260031749", (err, info) => responseFull(err, info, "steamID64ToFullInfo", "profile", "3urobeat", "76561198260031749"))
 
 steamidResolver.customUrlToFullInfo("3urobeat", (err, info) => responseFull(err, info, "customUrlToFullInfo", "profile", "3urobeat", "76561198260031749"))
 
