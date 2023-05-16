66d5de24892221b6b469391a6e75f662991c813a
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-05-16 19:01:22 +0200

    Add Promises support to all functions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * Created Date: 05.04.2023 19:04:56
  * Author: 3urobeat
  * 
- * Last Modified: 15.05.2023 23:49:38
+ * Last Modified: 16.05.2023 18:59:51
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2023 3urobeat <https://github.com/HerrEurobeat>
@@ -24,110 +24,162 @@ const { _parseXML }   = require("./helpers/parseXML.js");
 /**
  * Get the custom profile url of a user as String by their steamID64 or full URL
  * @param {String} steamID64 steamID64 or full URL of the user
- * @param {function} [callback] Called with `err` (String) and `customURL` (String) parameters on completion
+ * @param {function} [callback] Optional: Called with `err` (String) and `customURL` (String) parameters on completion
  */
-module.exports.steamID64ToCustomUrl = (steamID64, callback) => {
-    steamID64 = _parseParam(steamID64);
+module.exports.steamID64ToCustomUrl = (steamID64, callback = () => {}) => {
+    return new Promise((resolve, reject) => {
+        steamID64 = _parseParam(steamID64);
 
-    _parseXML(`https://steamcommunity.com/profiles/${steamID64}`)
-        .then(res => callback(null, res.customURL[0]))
-        .catch(err => callback(err, null));
+        _parseXML(`https://steamcommunity.com/profiles/${steamID64}`)
+            .then(res => {
+                resolve(res.customURL[0]);
+                callback(null, res.customURL[0]);
+            })
+            .catch(err => {
+                reject(err);
+                callback(err, null);
+            });
+    });
 };
 
 
 /**
  * Get the steamID64 of a user as String by their custom profile url or full URL
  * @param {String} customID Custom ID or full URL of the user as String
- * @param {function} [callback] Called with `err` (String) and `steamID64` (String) parameters on completion
+ * @param {function} [callback] Optional: Called with `err` (String) and `steamID64` (String) parameters on completion
  */
-module.exports.customUrlToSteamID64 = (customID, callback) => {
-    customID = _parseParam(customID);
+module.exports.customUrlToSteamID64 = (customID, callback = () => {}) => {
+    return new Promise((resolve, reject) => {
+        customID = _parseParam(customID);
 
-    _parseXML(`https://steamcommunity.com/id/${customID}`)
-        .then(res => callback(null, res.steamID64[0]))
-        .catch(err => callback(err, null));
+        _parseXML(`https://steamcommunity.com/id/${customID}`)
+            .then(res => {
+                resolve(res.steamID64[0]);
+                callback(null, res.steamID64[0]);
+            })
+            .catch(err => {
+                reject(err);
+                callback(err, null);
+            });
+    });
 };
 
 
 /**
  * Get the full information of a user as Object by their steamID64 or full URL
  * @param {String} steamID64 steamID64 or full URL of the user as String
- * @param {function} [callback] Called with `err` (String) and `info` (Object) parameters on completion
+ * @param {function} [callback] Optional: Called with `err` (String) and `info` (Object) parameters on completion
  */
-module.exports.steamID64ToFullInfo = (steamID64, callback) => {
-    steamID64 = _parseParam(steamID64);
+module.exports.steamID64ToFullInfo = (steamID64, callback = () => {}) => {
+    return new Promise((resolve, reject) => {
+        steamID64 = _parseParam(steamID64);
 
-    _parseXML(`https://steamcommunity.com/profiles/${steamID64}`)
-        .then(res => callback(null, res))
-        .catch(err => callback(err, null));
+        _parseXML(`https://steamcommunity.com/profiles/${steamID64}`)
+            .then(res => {
+                resolve(res);
+                callback(null, res);
+            })
+            .catch(err => {
+                reject(err);
+                callback(err, null);
+            });
+    });
 };
 
 
 /**
  * Get the full information of a user as Object by their custom profile url or full URL
  * @param {String} customID Custom ID or full URL of the user as String
- * @param {function} [callback] Called with `err` (String) and `info` (Object) parameters on completion
+ * @param {function} [callback] Optional: Called with `err` (String) and `info` (Object) parameters on completion
  */
-module.exports.customUrlToFullInfo = (customID, callback) => {
-    customID = _parseParam(customID);
+module.exports.customUrlToFullInfo = (customID, callback = () => {}) => {
+    return new Promise((resolve, reject) => {
+        customID = _parseParam(customID);
 
-    _parseXML(`https://steamcommunity.com/id/${customID}`)
-        .then(res => callback(null, res))
-        .catch(err => callback(err, null));
+        _parseXML(`https://steamcommunity.com/id/${customID}`)
+            .then(res => {
+                resolve(res);
+                callback(null, res);
+            })
+            .catch(err => {
+                reject(err);
+                callback(err, null);
+            });
+    });
 };
 
 
 /**
  * Get the group64ID of a group as String by groupURL or full URL
  * @param {String} groupURL Custom Name of the group or full URL as String
- * @param @param {function} [callback] Called with `err` (String) and `groupID64` (String) parameters on completion
+ * @param @param {function} [callback] Optional: Called with `err` (String) and `groupID64` (String) parameters on completion
  */
-module.exports.groupUrlToGroupID64 = (groupURL, callback) => {
-    groupURL = _parseParam(groupURL);
+module.exports.groupUrlToGroupID64 = (groupURL, callback = () => {}) => {
+    return new Promise((resolve, reject) => {
+        groupURL = _parseParam(groupURL);
 
-    _parseXML(`https://steamcommunity.com/groups/${groupURL}/memberslistxml`)
-        .then(res => callback(null, res.groupID64[0]))
-        .catch(err => callback(err, null));
+        _parseXML(`https://steamcommunity.com/groups/${groupURL}/memberslistxml`)
+            .then(res => {
+                resolve(res.groupID64[0]);
+                callback(null, res.groupID64[0]);
+            })
+            .catch(err => {
+                reject(err);
+                callback(err, null);
+            });
+    });
 };
 
 
 /**
  * Get the full information of a group as Object by groupURL or full URL
  * @param {String} groupURL Custom Name of the group or full URL as String
- * @param {function} [callback] Called with `err` (String) and `info` (Object) parameters on completion
+ * @param {function} [callback] Optional: Called with `err` (String) and `info` (Object) parameters on completion
  */
-module.exports.groupUrlToFullInfo = (groupURL, callback) => {
-    groupURL = _parseParam(groupURL);
+module.exports.groupUrlToFullInfo = (groupURL, callback = () => {}) => {
+    return new Promise((resolve, reject) => {
+        groupURL = _parseParam(groupURL);
 
-    _parseXML(`https://steamcommunity.com/groups/${groupURL}/memberslistxml`)
-        .then(res => callback(null, res))
-        .catch(err => callback(err, null));
+        _parseXML(`https://steamcommunity.com/groups/${groupURL}/memberslistxml`)
+            .then(res => {
+                resolve(res);
+                callback(null, res);
+            })
+            .catch(err => {
+                reject(err);
+                callback(err, null);
+            });
+    });
 };
 
 
 /**
  * Checks if the provided ID or full URL points to a valid sharedfile
  * @param {String} input Sharedfile ID of full URL
- * @param {function} [callback] Called with `err` (String) and `isValid` (Boolean) parameters on completion
+ * @param {function} [callback] Optional: Called with `err` (String) and `isValid` (Boolean) parameters on completion
  */
-module.exports.isValidSharedfileID = (input, callback) => {
-    // Precede input with URL if only the ID was provided
-    if (!input.includes("steamcommunity.com")) input = "https://steamcommunity.com/sharedfiles/filedetails/?id=" + input;
+module.exports.isValidSharedfileID = (input, callback = () => {}) => {
+    return new Promise((resolve, reject) => {
+        // Precede input with URL if only the ID was provided
+        if (!input.includes("steamcommunity.com")) input = "https://steamcommunity.com/sharedfiles/filedetails/?id=" + input;
 
-    try {
-        let output = "";
+        try {
+            let output = "";
 
-        require("https").get(input, (res) => {
-            res.on('data', (chunk) => output += chunk); // Append each chunk to output
+            require("https").get(input, (res) => {
+                res.on('data', (chunk) => output += chunk); // Append each chunk to output
 
-            res.on("end", () => {
-                // Check if output contains an error msg
-                let isInvalid = output.includes("There was a problem accessing the item") || output.includes("That item does not exist");
+                res.on("end", () => {
+                    // Check if output contains an error msg
+                    let isInvalid = output.includes("There was a problem accessing the item") || output.includes("That item does not exist");
 
-                callback(null, !isInvalid);
+                    resolve(!isInvalid);
+                    callback(null, !isInvalid);
+                });
             });
-        });
-    } catch (err) {
-        callback(err, null);
-    }
+        } catch (err) {
+            reject(err);
+            callback(err, null);
+        }
+    });
 };
